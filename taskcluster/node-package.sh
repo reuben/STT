81f09f11f98c83983e1f90709a67d5e6f432ba7c
Reuben Morais <reuben.morais@gmail.com>
2020-08-06 16:52:19 +0200

    wip

--- taskcluster/node-package.sh
+++ taskcluster/node-package.sh
@@ -7,4 +7,5 @@ source $(dirname "$0")/tc-tests-utils.sh
 mkdir -p ${TASKCLUSTER_ARTIFACTS} || true
 
 # NodeJS package
+ls -lh ${DS_ROOT_TASK}/DeepSpeech/ds/native_client/javascript/
 cp ${DS_ROOT_TASK}/DeepSpeech/ds/native_client/javascript/mozilla-voice-stt*.tgz ${TASKCLUSTER_ARTIFACTS}/
